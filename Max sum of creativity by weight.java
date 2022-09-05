9526455a9b88a615493dc43f3d7d8c42c8dfec6e
Laxmikant Verma <laxmikant91111@gmail.com>
2022-09-05 17:54:13 +0530

    Update Max sum of creativity by weight.java

--- Max sum of creativity by weight.java
+++ Max sum of creativity by weight.java
@@ -1,73 +1,52 @@
 import java.util.*;
 
 public class MyClass {
+    
+    static int sum=0;
+    
     public static void main(String args[]) {
       int k =3;
-      int[] c={8,9,7,9,2,1,4,5,6,9};
-      int[] h={1,2,2,2,1,2,3,2,1,3};
+      int[] c={7,3,22,1,2,33,90,21,3,54};
+      int[] h={1,1,1,2,2,2,3,3,4,4};
       
-      Map<Integer, String> m = new HashMap<>();
-      String cc="";
+      Map<Integer, Integer> m = new HashMap<>();
+      
       String hh="";
       
-      
-      int sum=0;
-      
       for(int i=0; i<c.length; i++){
-            
        
           if(!hh.contains(String.valueOf(h[i]))){
-              
               hh+=h[i]+"";
-              m.put(h[i], String.valueOf(c[i]));
-              
+              m.put(h[i], c[i]);
           }else{
-              
-              
-              if(!m.get(h[i]).contains(String.valueOf(c[i]))){
+              if(m.get(h[i])<c[i]){
                   
-                  if(m.get(h[i]).contains(",")){
-                      
-                      
-                      if(Integer.parseInt(m.get(h[i]).split(",")[m.get(h[i]).split(",").length-1])>c[i]){
-                          
-                          m.put(h[i], m.get(h[i])+","+c[i]);
-                      }else{
-                          
-                          cc=m.get(h[i]).substring(0, m.get(h[i]).length()-1)+""+c[i]+","+m.get(h[i]).substring(m.get(h[i]).length()-1, m.get(h[i]).length());
-                          
-                          m.put(h[i], cc);
-                      }
-                  }else{
-                      
-                      if(Integer.parseInt(m.get(h[i]))>c[i]){
-                          
-                          m.put(h[i], m.get(h[i])+","+c[i]);
-                      }else{
-                          
-                          m.put(h[i],c[i]+","+ m.get(h[i]));
-                      }
-                  }
-                  
-                  
+                  m.put(h[i], c[i]);
               }
-              
           }
           
           
       }
       
-      if(k==m.size()){
-          for(int j=1; j<=k; j++){
-            sum+=Integer.parseInt(m.get(j).split(",")[0]);
+      if(k<=m.size()){
+         
+        int arr[] = new int[m.size()];
+          
+        for(int j=1; j<=m.size(); j++){
+            arr[j-1]=m.get(j);
         }
+            
+        Arrays.sort(arr);
+        
+        for(int l=1; l<=k; l++){
+            sum+=arr[arr.length-l];
+        }
+            
       }else{
           sum=-1;
       }
       
-      
       System.out.println("Max Possible sum from all weight : "+sum);
-      
       
     }
 }
